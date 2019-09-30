be6ecbcc0e38617238ba818e7cd21ed0d228afa5
Thinh Tran <thinhtran@outlook.com.au>
2019-09-30 18:41:31 +0800

    init

---
+++ FacebookMessenger/FacebookMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookMessenger
+//
+//  Created by Thinh on 30/9/19.
+//  Copyright © 2019 VisualSharp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
