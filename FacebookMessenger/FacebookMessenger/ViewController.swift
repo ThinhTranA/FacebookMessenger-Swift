0a8faa8dcf6b380cbf28504d78f5de3072a38bc2
Thinh Tran <thinhtran@outlook.com.au>
2019-10-01 19:44:51 +0800

    Remove storyboard

--- FacebookMessenger/FacebookMessenger/ViewController.swift
+++ FacebookMessenger/FacebookMessenger/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //view.backgroundColor = .red;
+        collectionView?.backgroundColor = .blue;
     }
 
 
