06dcd2a8fa63e3224238c3f635b1342ab665154b
Thinh Tran <thinhtran@outlook.com.au>
2019-10-01 20:40:30 +0800

    flow layout

--- FacebookMessenger/FacebookMessenger/ViewController.swift
+++ FacebookMessenger/FacebookMessenger/ViewController.swift
@@ -8,15 +8,42 @@
 
 import UIKit
 
-class FriendsController: UICollectionViewController {
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    private let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //view.backgroundColor = .red;
-        collectionView?.backgroundColor = .blue;
+        collectionView?.backgroundColor = .white;
+        
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
     }
 
-
+    class FriendCell: UICollectionViewCell{
+        override init(frame: CGRect){
+            super.init(frame: frame)
+            setupViews()
+        }
+        
+        required init?(coder aDecoder: NSCoder){
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        func setupViews(){
+            backgroundColor = .blue;
+        }
+    }
 }
 
