9e71347567b16f0c8eb4fa135cc80a72e35e00f4
Thinh Tran <thinhtran@outlook.com.au>
2019-10-02 21:19:50 +0800

    Autolayout images and labels

--- FacebookMessenger/FacebookMessenger/ViewController.swift
+++ FacebookMessenger/FacebookMessenger/ViewController.swift
@@ -50,21 +50,75 @@ class FriendCell: BaseCell {
         return view
     }()
     
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Friend Name"
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your friend's messsage and something else..."
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12:05 pm"
+        label.textColor = .darkGray
+        label.textAlignment = .right
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    let hasReadImageView: UIImageView = {
+           let imageView = UIImageView()
+           imageView.contentMode = .scaleAspectFill
+           imageView.layer.cornerRadius  = 10
+           imageView.layer.masksToBounds = true
+           return imageView
+       }()
+    
     override func setupViews(){
         addSubview(profileImageView)
         addSubview(dividerLineView)
         
-        profileImageView.image = UIImage(named: "zuckprofile")
-        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        setupContainerView()
         
-        dividerLineView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.image = UIImage(named: "zuckprofile")
+        hasReadImageView.image = UIImage(named: "zuckprofile")
         
         addConstraintsWithFormat(format:"H:|-12-[v0(68)]", views: profileImageView)
-        addConstraintsWithFormat(format:"V:|-12-[v0(68)]", views: profileImageView)
+        addConstraintsWithFormat(format:"V:[v0(68)]", views: profileImageView)
+        
+        //center item in the parent container view
+        addConstraint(NSLayoutConstraint(item: profileImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
         
         addConstraintsWithFormat(format:"H:|-82-[v0]|", views: dividerLineView)
-        
         addConstraintsWithFormat(format:"V:[v0(1)]|", views: dividerLineView)
+    }
+    
+    private func setupContainerView(){
+        let containerView = UIView()
+        addSubview(containerView)
+        
+        addConstraintsWithFormat(format: "H:|-90-[v0]|", views: containerView)
+        addConstraintsWithFormat(format: "V:[v0(50)]", views: containerView)
+        
+        //center item in the parent container view
+        addConstraint(NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(hasReadImageView)
+        containerView.addConstraintsWithFormat(format: "H:|[v0][v1(80)]-12-|", views: nameLabel, timeLabel)
+        containerView.addConstraintsWithFormat(format: "V:|[v0][v1(24)]|", views: nameLabel, messageLabel)
+        
+        containerView.addConstraintsWithFormat(format: "H:|[v0]-8-[v1(20)]-12-|", views: messageLabel, hasReadImageView)
+        containerView.addConstraintsWithFormat(format: "V:|[v0(20)]", views: timeLabel)
+        containerView.addConstraintsWithFormat(format: "V:[v0(20)]|", views: hasReadImageView)
     }
 }
 
